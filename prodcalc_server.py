7720875bfb5100607f78a841f26fa0cded4de7c9
Benjamin Smith <bsmith.eng@gmail.com>
2014-11-03 19:32:12 -0500

    material cost breakdown, basic caching

--- prodcalc_server.py
+++ prodcalc_server.py
@@ -8,20 +8,31 @@ app = Flask(__name__)
 def index():
   return "EVE Production Calcs - Under Construction, Check Back Soon!"
 
-@app.route('/<int:typeID>')
-def show_item_by_id(typeID):
-  name = profitcalc.get_name_by_id(typeID)
-  prodcost = profitcalc.get_prod_cost_by_id(typeID)
-  value = profitcalc.get_item_value(typeID)
+@app.route('/<int:type_id>')
+def show_item_by_id(type_id):
+  name = profitcalc.get_name_by_id(type_id)
+  prodcost = profitcalc.get_prod_cost_by_id(type_id)
+  value = profitcalc.get_item_value(type_id)
+  #Inefficient for now - already called as part of get_prod_cost_by_id, decouple later
+  materials = profitcalc.get_item_materials(type_id)
   profit = value - prodcost
-  return render_template('item.html', name = name, prodcost = prodcost, value = value, profit = profit)
+  return render_template(
+    'item.html', name = name, prodcost = prodcost, 
+    value = value, profit = profit, materials = materials,
+    #absolute overkill here, interim solution only
+    prices = profitcalc.CACHED_PRICES)
 
-@app.route('/<itemName>')
-def show_item_by_name(itemName):
+@app.route('/<item_name>')
+def show_item_by_name(item_name):
   #A bit wasteful, since it'll do an extra lookup to find the name again
   #Still doing it this way for now to ensure code consistency rather than duplication
-  typeID = profitcalc.get_typeid_by_name(itemName)
-  return show_item_by_id(typeID)
+  type_id = profitcalc.get_type_id_by_name(item_name)
+  return show_item_by_id(type_id)
+
+@app.route('/favicon.ico')
+def favicon():
+  #favicon browser autoquery tries to trigger name-recognition without this
+  return ""
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=False)
+  app.run(host='0.0.0.0', debug=True)
--- profitcalc.py
+++ profitcalc.py
@@ -10,12 +10,14 @@ JITA = 30000142
 def fetch_func(url):
   return urllib2.urlopen(url).read()
 evec = evelink_evec.EVECentral(url_fetch_func = fetch_func)
+#Duct-tape caching solution to not make EVE-Central cry while debugging
+CACHED_PRICES = {}
 
 #PsycoPG/PostgreSQL setup
 conn = psycopg2.connect("dbname=EVE_Bulk user=cheezy52")
 cur = conn.cursor()
 
-def get_typeid_by_name(item_name):
+def get_type_id_by_name(item_name):
   #Returns the typeID for a single item name.  Must be an exact match.
   cur.execute('SELECT "typeID" FROM "invTypes" WHERE "typeName" = (%s);', [item_name])
   return cur.fetchone()[0]
@@ -27,31 +29,47 @@ def get_name_by_id(type_id):
 
 def get_item_materials(type_id):
   #Returns the materials required to construct an item, given the output item's typeID.
-  #Outputs are returned as a dict.  Keys: material typeIDs.  Values: quantity.
-  cur.execute('SELECT iam."materialTypeID", iam."quantity" FROM "industryActivityProducts" iap JOIN "industryActivityMaterials" iam ON iap."typeID" = iam."typeID" AND iam."activityID" = 1 JOIN "invTypes" it ON iam."materialTypeID" = it."typeID" WHERE "productTypeID" = (%s);', [type_id])
+  #Outputs are returned as a dict.  Keys: material typeIDs.  Values: (quantity, name).
+  cur.execute('SELECT iam."materialTypeID", iam."quantity", it."typeName" FROM "industryActivityProducts" iap JOIN "industryActivityMaterials" iam ON iap."typeID" = iam."typeID" AND iam."activityID" = 1 JOIN "invTypes" it ON iam."materialTypeID" = it."typeID" WHERE "productTypeID" = (%s);', [type_id])
   materials = {}
   for record in cur:
-    materials[record[0]] = record[1]
+    materials[record[0]] = (record[1], record[2])
   return materials
 
 def get_item_values(type_ids):
   #Returns the current market values for all typeIDs in the input array.
   #Outputs are returned as a dict.  Keys: material typeIDs.  Values: market avg. price.
-  response = evec.market_stats(type_ids, system=JITA)
   prices = {}
-  for key in response.keys():
-    prices[key] = response[key]['sell']['percentile']
-  return prices
+  if all (type_id in CACHED_PRICES.keys() for type_id in type_ids):
+    #Cache hit
+    for type_id in type_ids:
+      prices[type_id] = CACHED_PRICES[type_id]
+    return prices
+  else:
+    #Cache miss
+    response = evec.market_stats(type_ids, system=JITA)
+    print "EVE Central query fired"
+    for type_id in response.keys():
+      prices[type_id] = response[type_id]['sell']['percentile']
+      CACHED_PRICES[type_id] = prices[type_id]
+    return prices
 
 def get_item_value(type_id):
   #Convenience wrapper for returning a single item's value.
   #Returns the value directly, not as a dict.
-  return get_item_values([type_id])[type_id]
+  if type_id in CACHED_PRICES.keys():
+    #Cache hit
+    return CACHED_PRICES[type_id]
+  else:
+    #Cache miss
+    value = get_item_values([type_id])[type_id]
+    CACHED_PRICES[type_id] = value
+    return value
 
 def get_prod_cost_by_name(item_name):
   #Convenience wrapper for get_prod_cost_by_id
-  typeID = get_typeid_by_name(item_name)
-  return get_prod_cost_by_id(typeID)
+  type_id = get_type_id_by_name(item_name)
+  return get_prod_cost_by_id(type_id)
 
 def get_prod_cost_by_id(type_id):
   #Returns the cost to produce a given item from minerals.
@@ -60,10 +78,10 @@ def get_prod_cost_by_id(type_id):
   prices = get_item_values(materials.keys())
   prodcost = 0
   for mat in materials.keys():
-    prodcost += materials[mat]*prices[mat]
+    prodcost += materials[mat][0]*prices[mat]
   return prodcost
 
 def get_prod_profit_by_name(item_name):
   #Returns the gross profit given by producing an item over its current market value.
-  typeID = get_typeid_by_name(item_name)
-  return get_item_value(typeID) - get_prod_cost_by_id(typeID)
+  type_id = get_type_id_by_name(item_name)
+  return get_item_value(type_id) - get_prod_cost_by_id(type_id)
